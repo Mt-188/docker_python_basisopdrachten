2099110d970b1d2ddbf25b0ee000eaee3554360b
mt-188 <97130461@st.deltion.nl>
2026-09-16 13:59:41 +0200

    030 opdr 2

--- app/030_berekeningen/opdr_2/opdr_2.py
+++ app/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,14 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 102
 
-print()
+print (round((c * 9/5) + 32, 1))
+print (round((f - 32) * 5/9, 1))
+
+c = 62.2
+f = 96
+
+print (round((c * 9/5) + 32, 1))
+print (round((f - 32) * 5/9, 1))    
