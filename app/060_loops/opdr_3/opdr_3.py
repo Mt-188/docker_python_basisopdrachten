bd50c2682ec4a510f401bb714ff51c2652981d92
mt-188 <97130461@st.deltion.nl>
2026-09-24 15:34:37 +0200

    060 opdr 3

--- app/060_loops/opdr_3/opdr_3.py
+++ app/060_loops/opdr_3/opdr_3.py
@@ -1,11 +1,6 @@
 # Opdracht 3 input functie
 # Naam student:
-# Groep:
-
-# Hier komt je code...
-
-# Hier start de for-loop
-
+# Groep
 getallen = []
 
 for getal in range(3, 82, 3):
--- app/060_loops/opdr_4/opdr_4.py
+++ app/060_loops/opdr_4/opdr_4.py
@@ -5,5 +5,6 @@
 # Hier komt je code...
 
 # Hier start de for-loop
-
-my_list = []
+x = range(1,11)
+y = [4*i+7 for i in x]
+print(y)
